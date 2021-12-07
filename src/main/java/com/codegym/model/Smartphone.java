bd6bc5da005fba7486378533a5e82401fa3f3daa
ThaiVu54 <thai01654369440@gmail.com>
2021-12-07 17:11:37 +0700

    update phone ajax

---
+++ src/main/java/com/codegym/model/Smartphone.java
@@ -0,0 +1,64 @@
+package com.codegym.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="smartphones")
+public class Smartphone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String producer;
+    private String model;
+    private double price;
+
+    @ManyToOne
+    private Category category;
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return producer+": "+model+" with price "+price;
+    }
+
+}
